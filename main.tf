5029825c5e409e8cdff5e5f94112bb1810412ed8
Tomohiro-Kobayashi <tkobayashi.200@gmail.com>
2022-05-05 02:25:23 +0900

    practice section5

--- main.tf
+++ main.tf
@@ -14,5 +14,15 @@ provider "aws" {
 resource "aws_instance" "hello-world" {
   ami = "ami-02c3627b04781eada"
   instance_type = "t2.micro"
+
+  tags = {
+      Name = "HelloWorld"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+amazon-linux-extras install -y nginx1.12
+systemctl start nginx
+EOF
 }
 
