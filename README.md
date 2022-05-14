997238fc3662f48c022b44e1e30399a3c586317a
Tomohiro-Kobayashi <tkobayashi.200@gmail.com>
2022-05-15 01:27:29 +0900

    lecture 6

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Terraformコマンドのカンペ用リポジトリ
 
-## 使い方  
+## 事前準備 
 ディレクトリ配下に以下を記述したterraform.tfstateファイルを作成する.
 - aws_secret_key = "XXX"
 - aws_access_key = "XXX"
--- main.tf
+++ main.tf
@@ -1,22 +1,37 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {
+  type    = string
   default = "ap-northeast-1"
+}
+variable "envirnment" {
+  type = string
+}
+
+
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
 }
 
 provider "aws" {
   /* profile = "default" */
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 resource "aws_instance" "hello-world" {
-  ami = "ami-02c3627b04781eada"
+  ami           = "ami-02c3627b04781eada"
   instance_type = "t2.micro"
 
   tags = {
-      Name = "HelloWorld"
+    Name = "HelloWorld"
   }
 
   user_data = <<EOF
